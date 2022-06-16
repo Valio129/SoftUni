5e3dc914867bb371ba729ef975cde70afebb3f9e
Valio129 <88195264+Valio129@users.noreply.github.com>
2022-06-16 23:49:54 +0300

    Delete JS_PB directory

--- JS_PB/02. Multiplication Table.js
+++
@@ -1,7 +0,0 @@
-function multiplication(input) {
-    let numMultiply = Number(input[0]);
-    for (let index = 1; index <= 10; index++) {
-        console.log(`${index} * ${numMultiply} = ${index * numMultiply}`);
-
-    }
-} 
