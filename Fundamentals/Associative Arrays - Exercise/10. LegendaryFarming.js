acbe1706ff829c839ef790303096bdcbc43c3e27
Valio129 <88195264+Valio129@users.noreply.github.com>
2022-11-23 23:36:22 +0200

    Associative Arrays - Exercise

--- Fundamentals/Associative Arrays - Exercise/10. LegendaryFarming.js
+++ Fundamentals/Associative Arrays - Exercise/10. LegendaryFarming.js
@@ -33,21 +33,21 @@
 function farming(input) {
     let arr = input.toLowerCase().split(' ')
     let inventory = {
-        
         junk: new Map()
     }
     inventory['keyMaterials'] = new Map()
         .set('shards', 0)
         .set('fragments', 0)
         .set('motes', 0)
-        inventory['items'] = new Map()
+    inventory['items'] = new Map()
         .set('shards', 'Shadowmourne')
         .set('fragments', 'Valanyr')
         .set('motes', 'Dragonwrath')
 
-    for (let i = 0; i <= arr.length - 2; i += 2) {
-        let quantity = Number(arr[i])
-        let material = arr[i + 1]
+    while (arr.length > 0) {
+        let isLegendary = false;
+        let quantity = Number(arr.shift())
+        let material = arr.shift()
         if (inventory.keyMaterials.has(material)) {
             let currValue = inventory.keyMaterials.get(material)
             currValue += quantity
@@ -65,18 +65,35 @@ function farming(input) {
         }
         for (let token of inventory.keyMaterials.keys()) {
             let currMaterialValue = inventory.keyMaterials.get(token)
-            if (currMaterialValue > 250) {
+            if (currMaterialValue >= 250) {
                 inventory.keyMaterials
-                .set(material, Math.abs(currMaterialValue - quantity ))
-                console.log(`${inventory.items.get(token)} is obtained!`);
+                    .set(material, currMaterialValue - 250)
+                console.log(`${inventory.items.get(token)} obtained!`);
+                isLegendary = true;
                 break;
             }
         }
+        if (isLegendary) {
+            break;
+        }
     }
-    console.log();
+    let keySorted = Array.from(inventory.keyMaterials.keys())
+        .sort((a, b) => a.localeCompare(b))
+        .sort((a, b) => inventory.keyMaterials.get(b) - inventory.keyMaterials.get(a))
+    for (const key of keySorted) {
+        const value = inventory.keyMaterials.get(key)
+        console.log(`${key}: ${value}`);
+    }
+    let sortedJunk = [...inventory.junk.keys()].sort((a, b) => a.localeCompare(b))
+
+
+    for (const key of sortedJunk) {
+        const value = inventory.junk.get(key)
+        console.log(`${key}: ${value}`);
+    }
 }
 
 farming('3 Motes 5 stones 5 Shards 6 leathers 255 fragments 7 Shards')
 /*todo
 change values of obtained item
-***/ 
+***/
