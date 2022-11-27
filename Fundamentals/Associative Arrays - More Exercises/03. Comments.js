46831b58d02475941e1bddf939bd5a4779f17841
Valio129 <88195264+Valio129@users.noreply.github.com>
2022-11-27 22:29:04 +0200

    Associative Arrays - More Exercises

--- Fundamentals/Associative Arrays - More Exercises/03. Comments.js
+++ Fundamentals/Associative Arrays - More Exercises/03. Comments.js
@@ -1,63 +1,31 @@
-function solve(input) {
-    let usersList = []
-    let articlesList = []
-    /**
-     * article - {
-     * 
-     *      users - [title - comment]
-     * }
-     */
-    let articles = new Map()
-    for (let token of input) {
-        if (token.split(' ').includes('posts')) {
-            const splitSpace = token.split(' ')
-            const username = splitSpace[0]
-            const articleName = splitSpace[3]
-                .substring(0, splitSpace[3].length - 1)
-            // .substring(0, token[3].length - 1)
-            if (usersList.includes(username) && articlesList.includes(articleName)) {
-                let userContent = token.split(': ')[1]
-                const title = userContent.split(', ')[0]
-                const comment = userContent.split(', ')[1]
-                if (!articles.has(articleName)) {
-                    articles.set(articleName, new Map().set(username, [title, comment]))
-
-                } else {
-
-                    articles.get(articleName).set(username, [title, comment])
+function solve(inputs) {
+    let users = [];
+    let articles = [];
+    let commentsByArticle = {};
+    for (let input of inputs) {
+        if (input.startsWith('user ')) {
+            let user = input.split(' ')[1];
+            users.push(user);
+        } else if (input.startsWith('article ')) {
+            let article = input.split(' ')[1];
+            articles.push(article);
+        } else if (/^(.+) posts on (.+): (.+), (.+)$/.test(input)) {
+            let [match, user, article, title, content] = input.match(/^(.+) posts on (.+): (.+), (.+)$/);
+            if (users.includes(user) && articles.includes(article)) {
+                if (!commentsByArticle.hasOwnProperty(article)) {
+                    commentsByArticle[article] = [];
                 }
-            }
-        } else {
-            token = token.split(' ')
-            const action = token[0]
-            const value = token[1]
-            if (action === 'user') {
-                usersList.push(value)
-            } else {
-                articlesList.push(value)
+                commentsByArticle[article].push({user, title, content});
             }
         }
     }
-
-    //sort articles by size,
-    // for each article print comments ordered by the users key (alphabetically)
-    let sortedArticles = Array.from(articles.keys())
-        .sort((a, b) => a.localeCompare(b))
-        .sort((a, b) => {
-            let A = articles.get(a)
-            let B = articles.get(b)
-            return B.size - A.size
-        })
-    for (const articleName of sortedArticles) {
-        console.log(`Comments on ${articleName}`);
-        let articleKeys = Array.from(articles.get(articleName).keys())
-            .sort((a, b) => b.localeCompare(a))
-
-        for (const user of articleKeys) {
-            const title = articles.get(articleName).get(user)[0]
-            const content = articles.get(articleName).get(user)[1]
-            console.log(`--- From user ${user}: ${title} - ${content}`);
+    let sortedCommentsKeys = Object.keys(commentsByArticle).sort((a, b) => commentsByArticle[b].length - commentsByArticle[a].length);
+    for (const article of sortedCommentsKeys) {
+        console.log(`Comments on ${article}`);
+        let comments = commentsByArticle[article];
+        comments.sort((a, b) => a.user.localeCompare(b.user));
+        for (let comment of comments) {
+            console.log(`--- From user ${comment.user}: ${comment.title} - ${comment.content}`);
         }
     }
 }
-solve(['user aUser123', 'someUser posts on someArticle: NoTitle, stupidComment', 'article Books', 'article Movies', 'article Shopping', 'user someUser', 'user uSeR4', 'user lastUser', 'uSeR4 posts on Books: I like books, I do really like them', 'uSeR4 posts on Movies: I also like movies, I really do', 'someUser posts on Shopping: title, I go shopping every day', 'someUser posts on Movies: Like, I also like movies very much'])
--- Fundamentals/Associative Arrays - More Exercises/05. SoftUniStudents.js
+++ Fundamentals/Associative Arrays - More Exercises/05. SoftUniStudents.js
@@ -1,30 +1,59 @@
 function solveStudents(input) {
-    let courses = []
+    const coursePattern = /(?<name>[\w#]+):\s(?<cap>\d+)/;
+    const studentPattern = /(?<username>\w+)\[(?<credit>\d+)\] with email (?<email>\w+@\w+\.\w+) joins (?<course>[\w#]+)/;
+    let coursesObj = {}
+    let users = {}
     for (let token of input) {
-        if (token.split(': ').length == 2) {
-            let [courseName, capacity] = token.split(': ')
-            capacity = Number(capacity)
-            if (!courses.includes(obj => obj.courseName === courseName)) {
-                let courseObj = { courseName, capacity }
-                courses.push(courseObj)
+        if (coursePattern.test(token)) {
+            let [courseName, curCapacity] = token.split(': ')
+            curCapacity = Number(curCapacity)
+            //check if name is present in Obj
+            if (coursesObj.hasOwnProperty(courseName)) {
+                curCapacity += coursesObj[courseName].capacity
+                coursesObj[courseName].capacity = curCapacity
             } else {
-                courses[courses.indexOf(el.courseName === courseName)].capacity += capacity
-                // courses[courses.indexOf(el.courseName === courseName)] = {courseName, (capacity + curCap)}
+
+                coursesObj[courseName] = { capacity: curCapacity, users: [] }
             }
-        } else {
+        } else if (studentPattern.test(token)) {
             token = token.split(' ')
-            const user = token[0]
+            let user = token[0]
+            user = user.split('[')
+            let credits = Number(user[1].substring(0, user[1].indexOf(']')))
+            user = user[0]
             const email = token[3]
             const courseName = token[token.length - 1]
-            if (courses.includes(obj => obj.courseName === courseName)) {
-                let index = courses.indexOf( el => el.courseName === courseName)
-                let courseObj = courses[index]
-
+            if (coursesObj.hasOwnProperty(courseName) && coursesObj[courseName].capacity > 0 
+            && coursesObj[courseName].users.includes(user) === false) {
+                coursesObj[courseName].capacity -= 1
+                coursesObj[courseName].users.push(user)
+                users[user] = { email, credits }
             }
-            console.log();
         }
     }
-    console.log();
-
+    // +++ SORING PART
+    let sortedCourses = Array.from(Object.keys(coursesObj))
+        .sort((a, b) => {
+            let A = coursesObj[a].users.length
+            let B = coursesObj[b].users.length
+            return B - A
+        })
+    for (let course of sortedCourses) {
+        console.log(`${course}: ${coursesObj[course].capacity} places left`);
+        // TO DO: sort descending print credit ... check if sortedStudents has el's
+        if (coursesObj[course].users.length > 0) {
+            let sortedStudents = Array.from(coursesObj[course].users)
+                .sort((a, b) => {
+                    let A = users[a].credits
+                    let B = users[b].credits
+                    return B - A
+                })
+            for (const student of sortedStudents) {
+                const studCredits = users[student].credits
+                const studEmail = users[student].email
+                console.log(`--- ${studCredits}: ${student}, ${studEmail}`);
+            }
+        }
+    }
 }
 solveStudents(['JavaBasics: 2', 'user1[25] with email user1@user.com joins C#Basics', 'C#Advanced: 3', 'JSCore: 4', 'user2[30] with email user2@user.com joins C#Basics', 'user13[50] with email user13@user.com joins JSCore', 'user1[25] with email user1@user.com joins JSCore', 'user8[18] with email user8@user.com joins C#Advanced', 'user6[85] with email user6@user.com joins JSCore', 'JSCore: 2', 'user11[3] with email user11@user.com joins JavaBasics', 'user45[105] with email user45@user.com joins JSCore', 'user007[20] with email user007@user.com joins JSCore', 'user700[29] with email user700@user.com joins JSCore', 'user900[88] with email user900@user.com joins JSCore'])
