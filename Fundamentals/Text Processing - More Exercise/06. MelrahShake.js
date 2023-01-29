1631145370af3e8871468d28b23a0a51c4d45e26
Valio129 <88195264+Valio129@users.noreply.github.com>
2023-01-30 01:41:35 +0200

    .

--- Fundamentals/Text Processing - More Exercise/06. MelrahShake.js
+++ Fundamentals/Text Processing - More Exercise/06. MelrahShake.js
@@ -1,35 +1,35 @@
 function solve(array) {
     let text = array[0]
     let pattern = array[1]
-    while (text.includes(pattern)) {
-        let regex = new RegExp(`(?<patterns>${pattern})`, 'g')
+    while (text.includes(pattern) && pattern !== '') {
+        const regex = new RegExp(`(?<patterns>${pattern})`, 'g')
         let match = [...text.matchAll(regex)]
         // left match
         const leftMatchStartIndex = text.indexOf(pattern)
-        const leftMatchEndIndex = text.indexOf(pattern) + pattern.length
+        const leftMatchEndIndex = text.indexOf(pattern) + pattern.length - 1
         // right match
         const rightMatchStartIndex = match[match.length - 1].index
-        const rightMatchEndIndex = rightMatchStartIndex + pattern.length
+        const rightMatchEndIndex = rightMatchStartIndex + pattern.length - 1
         //cut matches
         if ((rightMatchStartIndex === rightMatchStartIndex &&
             leftMatchEndIndex === rightMatchEndIndex)) {
             break;
         }
         text = text.split('')
-        text.splice(rightMatchStartIndex, (rightMatchEndIndex - rightMatchStartIndex))
-        text.splice(leftMatchStartIndex, (leftMatchEndIndex - leftMatchStartIndex))
+        text.splice(rightMatchStartIndex, pattern.length)
+        text.splice(leftMatchStartIndex, pattern.length)
         text = text.join('')
         const letter = pattern[(parseInt(pattern.length / 2))]
-        pattern = pattern.replace(letter, '')
+        if (pattern.includes(letter)) {
+            pattern = pattern.replace(letter, '')
+        }
         console.log('Shaked it.');
     }
     console.log('No shake.');
     console.log(text);
 }
 
-solve(['astalavista baby',
-
-    'sta'])
+solve( ['stastasasssastasta', 'sta'] )
 solve(['##mtm!!mm.mm*mtm.#',
 
     'mtm'])
