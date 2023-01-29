f4308525ce7b25bc467c136b94538160707681b2
Valio129 <88195264+Valio129@users.noreply.github.com>
2023-01-30 01:13:35 +0200

    .

--- Fundamentals/Text Processing - More Exercise/06. MelrahShake.js
+++ Fundamentals/Text Processing - More Exercise/06. MelrahShake.js
@@ -1,16 +1,34 @@
 function solve(array) {
-    /*
-        get pattern 
-        loop through the array and find the first and the last match of
-        the pattern
-        create regex 
-    */
-   let text = 'astalavista baby'
-   let regex = /(?<pattern>sta)/gm
-   let match = [...text.matchAll(regex)]
-   console.log(text.substring(match[match.length - 1].index , match[match.length - 1][0].length));
+    let text = array[0]
+    let pattern = array[1]
+    while (text.includes(pattern)) {
+        let regex = new RegExp(`(?<patterns>${pattern})`, 'g')
+        let match = [...text.matchAll(regex)]
+        // left match
+        const leftMatchStartIndex = text.indexOf(pattern)
+        const leftMatchEndIndex = text.indexOf(pattern) + pattern.length
+        // right match
+        const rightMatchStartIndex = match[match.length - 1].index
+        const rightMatchEndIndex = rightMatchStartIndex + pattern.length
+        //cut matches
+        if ((rightMatchStartIndex === rightMatchStartIndex &&
+            leftMatchEndIndex === rightMatchEndIndex)) {
+            break;
+        }
+        text = text.split('')
+        text.splice(rightMatchStartIndex, (rightMatchEndIndex - rightMatchStartIndex))
+        text.splice(leftMatchStartIndex, (leftMatchEndIndex - leftMatchStartIndex))
+        text = text.join('')
+        const letter = pattern[(parseInt(pattern.length / 2))]
+        pattern = pattern.replace(letter, '')
+        console.log('Shaked it.');
+    }
+    console.log('No shake.');
+    console.log(text);
 }
+
 solve(['astalavista baby',
+
     'sta'])
 solve(['##mtm!!mm.mm*mtm.#',
 
