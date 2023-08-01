0f5d2632ba67d71d0f484b606fa8b32941376a1d
Valentin <88195264+Valio129@users.noreply.github.com>
2023-08-02 00:03:47 +0300

    /\
    /

--- JS Advanced/JS Advanced - Exercises/Objects and Composition - Exercise/09. SortedList.js
+++ JS Advanced/JS Advanced - Exercises/Objects and Composition - Exercise/09. SortedList.js
@@ -1,24 +1,30 @@
 function solve() {
     function createSortedList() {
         return {
-            nums: new Set(),
-            remove(num) {
-                this.nums.delete(num);
+            nums: [],
+            // sortedArr: this.nums.sort((a, b) => a - b),
+            size: 0,
+            remove(index) {
+                let sortedArr = this.nums.sort((a, b) => a - b);
+                let indexValue = sortedArr[index];
+                let delIndex = this.nums.indexOf(indexValue);
+                if (delIndex <= this.nums.length - 1 || delIndex >= 0) {
+                    if (this.nums[delIndex] !== undefined) {
+                        this.nums.splice(delIndex - 1, 1);
+                        this.size--;
+                    }
+                }
             },
             add(num) {
-                this.nums.add(num);
+                this.nums.push(num);
+                this.size++;
             },
-            get(num) {
-                // for (const el of this.nums.keys()) {
-                //     if (el == num) {
-                //         return this.nums.values[el];
-                //     }
-                // }
-                return this.nums.values()[num];
+            get(index) {
+                let sortedArr = this.nums.sort((a, b) => a - b);
+                if (index <= sortedArr.length - 1 || index >= 0) {
+                    return sortedArr[index];
+                }
             },
-            size() {
-                return this.nums.size;
-            }
         };
     }
     let list = createSortedList();
---
+++ JS Advanced/JS Advanced - Exercises/Objects and Composition - Exercise/test.js
@@ -0,0 +1,8 @@
+function solve() {
+   let list = new Set();
+   list.add(3) 
+   list.add(22) 
+   list.add(1)
+   console.log(); 
+}
+solve()
