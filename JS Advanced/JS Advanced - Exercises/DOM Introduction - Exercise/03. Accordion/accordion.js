afa6346185ad6de81319d47675ce5804968426c3
Valentin <88195264+Valio129@users.noreply.github.com>
2023-08-12 22:12:05 +0300

    aaa

--- JS Advanced/JS Advanced - Exercises/DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JS Advanced/JS Advanced - Exercises/DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    console.log('Less');
+    let contentEl = document.getElementById('extra');
+    let buttonEl = document.querySelector('span.button');
+    if (buttonEl.textContent == 'More') {
+        contentEl.style.display = 'block';
+        buttonEl.textContent = 'Less';
+    } else if (buttonEl.textContent == 'Less') {
+        contentEl.style.display = 'none';
+        buttonEl.textContent = 'More';
+    }
+
 }
--- JS Advanced/JS Advanced - Exercises/DOM Introduction - Exercise/04. Search in List/search.js
+++ JS Advanced/JS Advanced - Exercises/DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,23 @@
 function search() {
-   // TODO
+   let listEl = document.getElementById('towns');
+   let textBoxEl = document.getElementById('searchText');
+   let searchTown = textBoxEl.value;
+   let resultEl = document.getElementById('result');
+   let matchesCounter = Number(resultEl.textContent.split(' ')[0]);
+   if (listEl.textContent.includes(searchTown)) {
+      let townsArr = Array.from(document.getElementsByTagName('li'));
+      console.log(townsArr);
+      for (let i = 0; i < townsArr.length; i++) {
+         const currTown = townsArr[i].textContent;
+         if (currTown.includes(searchTown)) {
+            console.log(currTown);
+            townsArr[i].style.fontWeight = 'bold';
+            townsArr[i].style.textDecoration = 'underline';
+            matchesCounter++;
+         }
+      }
+   }
+   textBoxEl.value = '';
+   resultEl.textContent = `${matchesCounter} matches found`;
 }
+
