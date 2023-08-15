b06c62470fee2cd3549549a351b623d0ee558f37
Valentin <88195264+Valio129@users.noreply.github.com>
2023-08-15 23:46:05 +0300

    a

--- JS Advanced/JS Advanced - Exercises/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS Advanced/JS Advanced - Exercises/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,38 +1,12 @@
 function solve() {
-  let inputText = document.getElementById('input').value;
-  let sentancesArr = inputText.split('.');
-  // sentancesArr.pop();
-  sentancesArr = sentancesArr.filter(x => x !== '\n').filter(x => x !== '');
-  console.log(sentancesArr);
-  let outputArr = [];
-  let currParr = [];
-  let sentanceNum = 1;
-  for (let i = 0; i < sentancesArr.length; i++) {
-    let currSentance = sentancesArr[i];
-    console.log(currSentance + `, index: ${i}`);
-    if ((currSentance.length == 1)) {
-      if (sentancesArr[i + 1] !== undefined) {
-        currSentance = currSentance.concat(`.${sentancesArr[i + 1]}`);
-      } 
-      i++;
-      // sentanceNum;
-      // console.log(currSentance + '     new sentance');
-      // console.log(i);
-
-    }
-    if (currSentance.endsWith('.') == false) {
-      currSentance += '.';
-      // console.log(currSentance);
-    }
-    currParr.push(currSentance);
-    if (sentanceNum % 3 === 0 || (sentancesArr[i + 1] == undefined)) {
-      console.log('end parr');
-      outputArr.push(`<p> ${currParr.join(' ')} </p>`);
-      currParr = [];
-    }
-    sentanceNum++;
-
-  }
+  let inputTextEl = document.getElementById('input');
+  let sentancesArr = inputTextEl.value.split('.').filter(s => s !== '');
   let resultEl = document.getElementById('output');
-  resultEl.innerHTML = `${outputArr.join(' ')}`;
+
+  while (sentancesArr.length > 0) {
+    let sentance = sentancesArr.splice(0, 3).join('. ') + '.';
+    let p = document.createElement('p');
+    p.textContent = sentance;
+    resultEl.appendChild(p);
+  }
 }
