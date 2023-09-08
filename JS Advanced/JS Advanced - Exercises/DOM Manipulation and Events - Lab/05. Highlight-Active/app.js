4ab9c49f817a700f4e36eeed3f833d75d5da652c
Valentin <88195264+Valio129@users.noreply.github.com>
2023-09-09 00:35:15 +0300

    /

--- JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,12 @@
 function focused() {
-    console.log('TODO:...');
+    let container = Array.from(document.querySelectorAll('div div'));
+    container.forEach(element => {
+        let input = element.children[1];
+        input.addEventListener('focus', (e) => {
+            let el = e.currentTarget;
+            el.parentNode.setAttribute('class','focused')
+            console.log(e.target);
+        });
+    });
+
 }
