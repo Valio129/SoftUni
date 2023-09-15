d4f886e6ccd5f413bbf428a9f2b9d24fecc0c50f
Valentin <88195264+Valio129@users.noreply.github.com>
2023-09-15 10:53:11 +0300

    .

--- JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,20 +1,23 @@
 function lockedProfile() {
-    const btns = Array.from(document.querySelectorAll('.profile button'))
-        .forEach(btn => btn.addEventListener('click', revealData));
-    function revealData(e) {
+    Array.from(document.querySelectorAll('.profile button'))
+        .forEach(btn => btn.addEventListener('click', toggle));
+    function toggle(e) {
         const el = e.target;
         const isActive = el.parentElement.querySelector('input[type="radio"][value="unlock"').checked;
         if (isActive) {
-            const hiddenInfo = el.parentElement.querySelector('div');
-            if (el.textContent = 'Show more') {
-                hiddenInfo.style.display = 'block';
+            //  = Array
+            // .from(el.parentElement.querySelectorAll('div'))
+            // .find(d => d.id.includes('HiddenFields'));
+            const hiddenInfo = el.parentElement.querySelector('div')
+            if (el.textContent == 'Show more') {
+                hiddenInfo.style.display = 'block'  ;
                 el.textContent = 'Hide it';
                 // console.log(el.textContent);
-                console.log(hiddenInfo);
-            } else {
+                // console.log(hiddenInfo);
+            } else if (el.textContent == 'Hide it'){
                 hiddenInfo.style.display = '';
                 el.textContent = 'Show more';
-                // console.log(el.textContent);
+                // console.log('YES');
             }
         }
     }
