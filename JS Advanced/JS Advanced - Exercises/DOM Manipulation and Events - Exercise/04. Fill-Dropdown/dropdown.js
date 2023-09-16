8d9e65555f9977b54ee2127a74b4bc5154936f58
Valentin <88195264+Valio129@users.noreply.github.com>
2023-09-16 16:10:15 +0300

    /

--- JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let [inputText, inputValue] = Array.from(document.querySelectorAll('input'));
+    const newEl = document.createElement('option');
+    newEl.textContent = inputText.value;
+    newEl.value = inputValue.value;
+    const menu = document.querySelector('div select[id="menu"]');
+    menu.appendChild(newEl);
+    console.log(inputText);
+    inputText.value = '';
+    inputValue.value = '';
+
 }
