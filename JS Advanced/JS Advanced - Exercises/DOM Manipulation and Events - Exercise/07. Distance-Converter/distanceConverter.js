37f4ad887e994b59bf20a626bbef80d1e785093d
Valentin <88195264+Valio129@users.noreply.github.com>
2023-09-20 23:36:53 +0300

    /

--- JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
+++ JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,26 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const [input, output] = document.querySelectorAll('input[type="text"]');
+    const button = document.querySelector('input#convert');
+    // console.log(button);
+    // console.log(output);
+    const converter = {
+        'km': 1000,
+        'm': 1,
+        'cm': 0.01,
+        'mm': 0.001,
+        'mi': 1609.34,
+        'yrd': 0.9144,
+        'ft': 0.3048,
+        'in': 0.0254,
+    };
+    button.addEventListener('click', convert);
+    function convert(e) {
+        const selectedInputUnit = document.getElementById('inputUnits').value;
+        let value = Number(input.value);
+        let valueToM = value * converter[selectedInputUnit];
+        // console.log(valueToM);
+        const selectedOutUnit = document.getElementById('outputUnits').value;
+        const outValue = valueToM / converter[selectedOutUnit];
+        output.value = outValue;
+    }
 }
--- JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
+++ JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
@@ -1,4 +1,63 @@
 function solve() {
+    const [checkBtn, clearBtn] = Array.from(document.querySelectorAll('#exercise table tfoot td button'));
+    console.log(checkBtn);
+    console.log(clearBtn);
+    const tbody = Array.from(
+        document.querySelectorAll('#exercise table tbody tr')
+    );
+    // console.log(rows);
+    checkBtn.addEventListener('click', checkResult);
+    clearBtn.addEventListener('click', clear);
+    function hasDuplicates(array) {
+        let isDuplicate = false;
+        return ((new Set(array)).size !== array.length) 
+    }
+    function clear(e) {
+        for (let index = 0; index < tbody.length; index++) {
+            let tr = Array.from(tbody[index].children);
+            for (let trIndex = 0; trIndex < tr.length; trIndex++) {
+                const td = tr[trIndex];
+                td.children[0].value = '';
+                const table = document.getElementsByTagName('table')[0];
+                table.style.border = null;
+                let out = document.querySelector('#check p');
+                out.textContent = '';
+            }
 
-    //TODO...
+        }
+    }
+    function checkResult(e) {
+        for (let index = 0; index < tbody.length; index++) {
+            let tr = Array.from(tbody[index].children);
+            for (let trIndex = 0; trIndex < tr.length; trIndex++) {
+                const td = tr[trIndex];
+                let col = [];
+                for (let i = 0; i < tbody.length; i++) {
+                    const firstTd = tbody[i].children[trIndex];
+                    col.push(firstTd);
+                }
+                console.log([td]);
+                console.log(col);
+                const row = tr.map(cell => cell = cell.children[0].value);
+                console.log(row);
+                console.log(tr);
+                const column = col.map(cell => cell = cell.children[0].value);
+                console.log(column);
+                if (hasDuplicates(row) || hasDuplicates(Array.from(column))) {
+                    const table = document.getElementsByTagName('table')[0];
+                    table.style.border = '2px solid red';
+                    let out = document.querySelector('#check p');
+                    out.textContent = 'NOP! You are not done yet...';
+                } else {
+                    const table = document.getElementsByTagName('table')[0];
+                    table.style.border = '2px solid green';
+                    let out = document.querySelector('#check p');
+                    out.textContent = 'You solve it! Congratulations!';
+                }
+            }
+
+        }
+
+    }
+
 }
