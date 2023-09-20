7478e918c811bf7537ba998a2cbfabefc90acb50
Valentin <88195264+Valio129@users.noreply.github.com>
2023-09-20 17:25:58 +0300

    .

--- JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/06. Furniture/solution.js
+++ JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/06. Furniture/solution.js
@@ -1,40 +1,92 @@
-// tbody tr td,td,td
-// get data from textarea and JSON parce it 
-// for each obj -> generate one row 
-// each key -> generate: td: picture append it  td: name append it and so on
-// append the row to the table
+/*   
+                    INPUT ARRAY
+    [{"name": "Sofa", "img": "https://res.cloudinary.com/maisonsdumonde/image/upload/q_auto,f_auto/w_200/img/grey-3-seater-sofa-bed-200-13-0-175521_9.jpg", "price": 150, "decFactor": 1.2}]
+    
+    */
 function solve() {
+    const [input, output] = Array.from(document.getElementsByTagName('textarea'));
+    const [generateBtn, buyBtn] = Array.from(document.getElementsByTagName('button'));
+    const tableData = document.getElementsByTagName('table')[0];
+    const itemList = (
+        tableData.getElementsByTagName('tbody')[0]
+            .children
+    );
+    const lib = {
+        img: ['img', 'src'],
+        name: 'p',
+        price: 'p',
+        decFactor: 'p',
+    };
+    console.log(itemList);
 
-  const main = (document.querySelector('#container div#exercise'));
-  const generateBtn = main.querySelector('h1').nextElementSibling.nextElementSibling;
-  const itemList = main.querySelector('div.wrapper div.card-wrapper table tbody');
-  console.log(itemList);
-  generateBtn.addEventListener('click', generateItems);
-  function generateItems(e) {
-    const textAreaData = e.target.parentNode.querySelector('textarea').value;
-    const inputItemsArray = Array.from(JSON.parse(textAreaData));
-    // console.log(inputItems);
-    for (const itemObj of inputItemsArray) {
-      const tbRow = document.createElement('tr');
-      for (const key in itemObj) {
-        const element = itemObj[key];
-        const td = document.createElement('td');
-        if (key =='img') {
-          td.appendChild(document.createElement('img').setAttribute('href',`${element}`))
-        } else if(key == 'price' || key == 'decFactor') {
-          const data = document.createElement('p')
-          data.textContent = Number(element);
-          td.appendChild(data);
-        } else {
-          const data = document.createElement('p')
-          td.appendChild(data);
+    generateBtn.addEventListener('click', generate);
+    buyBtn.addEventListener('click', buy);
+
+
+    function generate(e) {
+        const inputArray = Array.from(JSON.parse(input.value));
+        // console.log(itemList);
+        for (const arrObject of inputArray) {
+            addToList(createItemFrom(arrObject), itemList);
+            console.log(createItemFrom(arrObject));
         }
-        tbRow.appendChild(td);
-      }
-      itemList.appendChild(tbRow)
-
     }
 
-  }
-  //TODO...
+    function createItemFrom(inputObj) {
+        const outputObj = {};
+        for (const key in lib) {
+            outputObj[key] = inputObj[key];
+        }
+        // console.log(outputObj);
+        return outputObj;
+    }
+    function generateEl(tag, atr, value) {
+        const td = document.createElement('td');
+        const el = document.createElement(tag);
+        if (atr) {
+            el.setAttribute(atr, value);
+        } else {
+            el.textContent = value;
+        }
+        td.appendChild(el);
+        console.log(typeof td);
+        return td;
+    }
+    function addToList(finalObj, list) {
+        console.log(list);
+        const tr = document.createElement('tr');
+        for (const key in finalObj) {
+            let child = generateEl(lib[key][0], lib[key][1], finalObj[key]);
+            tr.appendChild(child);
+
+        }
+        const checkCell = document.createElement('td');
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkCell.appendChild(checkbox);
+        tr.appendChild(checkCell);
+        document.querySelector('table.table tbody').appendChild(tr);
+        console.log('here');
+
+    }
+    function buy(e) {
+        let purchased = [];
+        let totalCost = 0;
+        let decF = 0;
+        let checked = Array.from(document.querySelectorAll('table input[type ="checkbox"]:checked'));
+        console.log('checked checkboxes', checked);
+        for (const itemRow of checked) {
+            console.log(itemRow.parentNode);
+            const itemName = itemRow.parentNode.parentElement.children[1].textContent;
+            const itemPrice = Number(itemRow.parentNode.parentElement.children[2].textContent);
+            const itemDecFactor = Number(itemRow.parentNode.parentElement.children[3].textContent);
+            if (!purchased.includes(itemName)) {
+                purchased.push(itemName);
+            }
+            totalCost += itemPrice;
+            decF += itemDecFactor;
+        }
+        decF /= checked.length;
+        output.value = `Bought furniture: ${purchased.join(', ')}\nTotal price: ${totalCost.toFixed(2)}\nAverage decoration factor: ${decF}`;
+    }
 }
