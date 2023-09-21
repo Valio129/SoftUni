4571ca2294fd2aba2c8ccce912024317a381877d
Valentin <88195264+Valio129@users.noreply.github.com>
2023-09-21 15:29:08 +0300

    .

--- JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
+++ JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
@@ -1,17 +1,30 @@
+// TO DO: fix not showing false when there is empty cell
 function solve() {
+
     const [checkBtn, clearBtn] = Array.from(document.querySelectorAll('#exercise table tfoot td button'));
-    console.log(checkBtn);
-    console.log(clearBtn);
+    // console.log(checkBtn); 
+    // console.log(clearBtn);
     const tbody = Array.from(
         document.querySelectorAll('#exercise table tbody tr')
     );
     // console.log(rows);
     checkBtn.addEventListener('click', checkResult);
     clearBtn.addEventListener('click', clear);
+
+    // function hasDuplicates(array) {
+    //     let isDuplicate = false;
+    //     let set = new Set(array);
+    //     if ((set.size !== array.size) || array.includes('')) {
+    //         isDuplicate = true;
+    //     }
+    //     console.log(isDuplicate);
+    //     return isDuplicate;
+
+    // }
     function hasDuplicates(array) {
-        let isDuplicate = false;
-        return ((new Set(array)).size !== array.length) 
+        return (new Set(array)).size !== array.length;
     }
+
     function clear(e) {
         for (let index = 0; index < tbody.length; index++) {
             let tr = Array.from(tbody[index].children);
@@ -19,13 +32,14 @@ function solve() {
                 const td = tr[trIndex];
                 td.children[0].value = '';
                 const table = document.getElementsByTagName('table')[0];
-                table.style.border = null;
+                table.style.border = 'none';
                 let out = document.querySelector('#check p');
                 out.textContent = '';
             }
 
         }
     }
+
     function checkResult(e) {
         for (let index = 0; index < tbody.length; index++) {
             let tr = Array.from(tbody[index].children);
@@ -36,23 +50,27 @@ function solve() {
                     const firstTd = tbody[i].children[trIndex];
                     col.push(firstTd);
                 }
-                console.log([td]);
-                console.log(col);
+                // console.log([td]);
+                // console.log(col);
                 const row = tr.map(cell => cell = cell.children[0].value);
-                console.log(row);
-                console.log(tr);
+                // console.log(row);
+                // console.log(tr);
                 const column = col.map(cell => cell = cell.children[0].value);
-                console.log(column);
-                if (hasDuplicates(row) || hasDuplicates(Array.from(column))) {
-                    const table = document.getElementsByTagName('table')[0];
-                    table.style.border = '2px solid red';
-                    let out = document.querySelector('#check p');
-                    out.textContent = 'NOP! You are not done yet...';
-                } else {
-                    const table = document.getElementsByTagName('table')[0];
-                    table.style.border = '2px solid green';
-                    let out = document.querySelector('#check p');
-                    out.textContent = 'You solve it! Congratulations!';
+                // console.log(column);
+                if (row.every(e => e !== '') && Array.from(column).every(e => e !== '')) {
+                    if ((hasDuplicates(row)) || (hasDuplicates(Array.from(column)))) {
+                        const table = document.getElementsByTagName('table')[0];
+                        table.style.border = '2px solid red';
+                        let out = document.querySelector('#check p');
+                        out.textContent = 'NOP! You are not done yet...';
+                        out.style.color = 'red';
+                    } else if (hasDuplicates(row) == false && hasDuplicates(Array.from(column)) == false) {
+                        const table = document.getElementsByTagName('table')[0];
+                        table.style.border = '2px solid green';
+                        let out = document.querySelector('#check p');
+                        out.textContent = 'You solve it! Congratulations!';
+                        out.style.color = 'green';
+                    }
                 }
             }
 
