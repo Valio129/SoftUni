e18bbd0c76c2fdda4fa7363223b34ac7dd2b3456
Valentin <88195264+Valio129@users.noreply.github.com>
2023-09-21 16:25:36 +0300

    .

--- JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
+++ JS Advanced/JS Advanced - Exercises/DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-  //TODO...
+  let i = 0;
+  let score = 0;
+  const sections = [...document.querySelectorAll('#quizzie section')];
+  sections[i].addEventListener('click', change);
+  const result = document.querySelector('#quizzie #results');
+  const answers = [0, 1, 0];
+  function change(e) {
+    const btns = [...document.querySelectorAll(`#quizzie section:nth-of-type(${i + 1}) ul li div p.answer-text`)];
+    console.log(btns);
+    const el = e.target;
+    if (el == btns[answers[i]]) {
+      score++;
+      console.log('true');
+    } else {
+      console.log('false');
+    }
+    sections[i].style.display = 'none';
+    i++;
+    if (sections[i] == undefined) {
+      result.style.display = 'block';
+      if (score == 3) {
+        result.children[0].children[0].textContent = "You are recognized as top JavaScript fan!";
+      } else {
+        result.children[0].children[0].textContent = `You have ${score} right answers`;
+      }
+
+      return;
+    }
+    sections[i].style.display = 'block';
+    sections[i].addEventListener('click', change);
+  }
 }
