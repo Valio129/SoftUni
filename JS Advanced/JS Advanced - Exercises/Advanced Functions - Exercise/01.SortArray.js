ac6e627367e1108e8847c0a1844ae38757b2f8c7
Valentin <88195264+Valio129@users.noreply.github.com>
2023-09-27 15:24:27 +0300

    Fixed  incorrect output
    Had to edit the initial variable values.

--- JS Advanced/JS Advanced - Exercises/Advanced Functions - Exercise/01.SortArray.js
+++ JS Advanced/JS Advanced - Exercises/Advanced Functions - Exercise/01.SortArray.js
@@ -1,6 +1,5 @@
 function sortArr(arr ,cmd) {
     if(cmd === 'asc') return (arr.sort((a,b) => a-b));
     else return (arr.sort((a,b) => b-a));
-
 }
 sortArr([14, 7, 17, 6, 8], 'desc')
--- JS Advanced/JS Advanced - Exercises/Advanced Functions - Exercise/03.Fibonacci.js
+++ JS Advanced/JS Advanced - Exercises/Advanced Functions - Exercise/03.Fibonacci.js
@@ -1,20 +1,23 @@
 function getFibonator() {
-    let first = 0;
-    let second = 1;
-    let sum;
-    return function () {
-       sum = first + second
-        first = second
-        second = sum
-        return sum
-    };
+    let first = 1;
+    let second = 0;
+     function getNum () {
+        let temp1 = first + second  ;
+         first = second;
+         second = temp1;
+        return temp1;
+    }
+    return getNum;
 }
+
+
+
 let fib = getFibonator();
-                    // 0    first
-console.log(fib()); // 1    second -> first
-console.log(fib()); // 1    sum ->    second
-console.log(fib()); // 2               sum
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
 console.log(fib()); // 3
 console.log(fib()); // 5
 console.log(fib()); // 8
 console.log(fib()); // 13
+
