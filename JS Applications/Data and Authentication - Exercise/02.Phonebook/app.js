ed5a7b1f3600acd4e6fe17cf675ba417507c4bd3
Valentin <88195264+Valio129@users.noreply.github.com>
2023-11-12 14:22:21 +0200

    03. Students

--- JS Applications/Data and Authentication - Exercise/02.Phonebook/app.js
+++ JS Applications/Data and Authentication - Exercise/02.Phonebook/app.js
@@ -1,5 +1,56 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnCreate').addEventListener('click', onCreate);
+    document.getElementById('btnLoad').addEventListener('click', onLoad);
+    document.getElementById('btnCreate').addEventListener('click', onCreate);
+    list.addEventListener('click', onDelete)
+    // fetch('http://localhost:3030/jsonstore/phonebook').
 }
-
+const list = document.getElementById('phonebook');
 attachEvents();
+
+async function onCreate() {
+    const person = document.getElementById('person');
+    const phone = document.getElementById('phone');
+    // console.log(person, p);
+    const response = await fetch('http://localhost:3030/jsonstore/phonebook', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ person: person.value, phone: phone.value })
+    });
+    person.value = ''
+    phone.value = ''
+    console.log(await response.json());
+    onLoad()
+}
+async function onLoad() {
+    list.innerHTML = '';
+    const response = await fetch('http://localhost:3030/jsonstore/phonebook');
+    const data = await (response.json());
+    console.log(data);
+    Object.entries(data).map(([k, v]) => createContact(v, k)).forEach(c => list.appendChild(c));
+
+}
+function createContact(contact, id) {
+
+    console.log(contact , id);
+    const li = document.createElement('li');
+    li.textContent = ``;
+    li.innerHTML = `${contact.person}: ${contact.phone}<button data-id="${id}">Delete</button>`;
+    li.id = 'phonebook';
+    return li;
+}
+async function onDelete(event) {
+    console.log(event.target);
+    if (event.target.tagName == 'BUTTON') {
+        // console.log('clicked button: http://localhost:3030/jsonstore/'+ event.target.dataset.id);
+        const response = await fetch('http://localhost:3030/jsonstore/phonebook/' + event.target.dataset.id, {
+            method: 'DELETE',
+        
+        })
+        const result = await response.json()
+        console.log(result);
+       await event.target.parentNode.remove();
+    }
+}
--- JS Applications/Data and Authentication - Exercise/03.Students/app.js
+++ JS Applications/Data and Authentication - Exercise/03.Students/app.js
@@ -1,1 +1,36 @@
-console.log('TODO...');
+
+const table = document.getElementById('results');
+window.addEventListener('load', onLoad);
+document.getElementById('form').addEventListener('submit', onSubmit);
+async function onLoad() {
+    const response = await fetch('http://localhost:3030/jsonstore/collections/students');
+    const studentsData = await response.json();
+    table.querySelector('tbody').replaceChildren(...Object.values(studentsData).map(createRow));
+}
+function createRow(student) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${student.firstName}</td>
+    <td>${student.lastName}</td><td>${student.facultyNumber}</td><td>${student.grade}</td>`;
+    return tr;
+}
+async function onSubmit(event) {
+    event.preventDefault()
+    const data = new FormData(event.target);
+    console.log(data);
+    const studentData = {
+        firstName: data.get('firstName'),
+        lastName: data.get('lastName'),
+        facultyNumber: data.get('facultyNumber'),
+        grade: data.get('grade')
+    };
+    const response = await fetch('http://localhost:3030/jsonstore/collections/students',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(studentData)
+    }); 
+    const result = await response.json()
+    console.log(result);
+    onLoad()
+}
